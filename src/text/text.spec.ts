196269e2b9960e6700d731e81c59896aafc582bc
Indri Muska <indrimuska@gmail.com>
2018-04-05 20:57:50 +0200

    Text: add test for `disabled` prop

--- src/text/text.spec.ts
+++ src/text/text.spec.ts
@@ -1,24 +1,24 @@
-import { prepareTest, findElement } from '../utility/test';
+import { prepareTest, findElement, detectChanges } from '../utility/test';
 import { TextComponent } from './text';
-import { fakeAsync, tick } from '@angular/core/testing';
+import { fakeAsync } from '@angular/core/testing';
 
 describe('TextComponent', () => {
     it('should set value', fakeAsync(() => {
         // preparation
         const value = 'John Doe';
         const fixture = prepareTest(TextComponent, { value });
-        const inputEl = findElement<HTMLInputElement>(fixture, 'input');
+        const inputEl = findElement<HTMLInputElement>('input');
 
         // testing
         inputEl.dispatchEvent(new Event('input'));
-        tick();
+        detectChanges();
         expect(inputEl.value).toBe(value);
 
         // test value update
         const newValue = 'Foo Bar';
         fixture.componentInstance.value = newValue;
         inputEl.dispatchEvent(new Event('input'));
-        tick();
+        detectChanges();
         expect(inputEl.value).toBe(newValue);
     }));
 
@@ -26,9 +26,30 @@ describe('TextComponent', () => {
         // preparation
         const placeholder = 'Type something...';
         const fixture = prepareTest(TextComponent, { placeholder });
-        const inputEl = findElement<HTMLInputElement>(fixture, 'input');
+        const inputEl = findElement<HTMLInputElement>('input');
 
         // testing
         expect(inputEl.placeholder).toBe(placeholder);
     });
+
+    it('should enable/disable the input', fakeAsync(() => {
+        // preparation
+        let disabled = false;
+        const fixture = prepareTest(TextComponent, { disabled });
+        const inputEl = findElement<HTMLInputElement>('input');
+
+        // testing
+        expect(inputEl.disabled).toBe(disabled);
+        
+        disabled = true;
+        fixture.componentInstance.disabled = disabled;
+        detectChanges();
+        expect(inputEl.disabled).toBe(disabled);
+        
+        disabled = false;
+        fixture.componentInstance.disabled = disabled;
+        fixture.detectChanges();
+        detectChanges();
+        expect(inputEl.disabled).toBe(disabled);
+    }));
 });
--- src/utility/test.ts
+++ src/utility/test.ts
@@ -1,9 +1,11 @@
 import { Type } from '@angular/core';
-import { TestBed, ComponentFixture, tick as ngTick } from '@angular/core/testing';
+import { TestBed, ComponentFixture, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
+let fixture: ComponentFixture<any>;
+
 export const prepareTest = <T>(component: Type<T>, props?: Partial<T>) => {
-    const fixture = TestBed.createComponent(component);
+    fixture = TestBed.createComponent(component);
     if (props) {
         Object.keys(props).forEach(key => {
             fixture.componentInstance[key] = props[key];
@@ -14,6 +16,11 @@ export const prepareTest = <T>(component: Type<T>, props?: Partial<T>) => {
     return fixture;
 };
 
-export const findElement = <T extends HTMLElement>(fixture: ComponentFixture<any>, query: string): T => {
+export const findElement = <T extends HTMLElement>(query: string): T => {
     return fixture.debugElement.query(By.css(query)).nativeElement as T;
+};
+
+export const detectChanges = () => {
+    fixture.detectChanges();
+    tick();
 };
